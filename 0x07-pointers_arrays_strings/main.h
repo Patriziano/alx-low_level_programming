bcfab70486cefb8542014037423a14102c789aff
Patriziano <onahpatrick75@gmail.com>
2023-03-06 15:20:13 -0800

    function that prints a chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,6 +9,8 @@ unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
 
+int _putchar(char);
+
 char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);

e95f282bda96154612703aefd11a2ad13a9871fc
Patriziano <onahpatrick75@gmail.com>
2023-03-10 14:12:53 -0800

    A program that prints its name followed by a new name using argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
 *
 *Return: 0 success.
 */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);

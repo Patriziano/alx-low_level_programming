84314037791434b963d90ee5437a562ffa88ae58
Patriziano <onahpatrick75@gmail.com>
2023-03-22 10:42:07 -0700

    a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+*print_name - function that prints a name
+*@name: The name input
+*@f: function pointer
+*
+*Return: Nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+Project on Function pointers on C programming

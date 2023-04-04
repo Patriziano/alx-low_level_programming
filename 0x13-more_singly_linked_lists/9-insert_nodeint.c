daa911a802953f6aa668b932c17f5abff399ec0b
Patriziano <onahpatrick75@gmail.com>
2023-04-04 14:56:20 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	latest = malloc(sizeof(listint_t));
-	if (head == NULL)
+	if (latest == NULL)
 	{
 		return (NULL);
 	}

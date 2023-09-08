b30f42be7a4d9a329fba8197f2d0fc04fb8600b2
Patriziano <onahpatrick75@gmail.com>
2023-09-08 16:43:36 -0700

    error fixed

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,20 +8,22 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int idx;
 	hash_node_t *node;
+	char *blank;
 
 	if (ht == NULL)
 		return;
 
 	printf("{");
+	blank = "";
+
 	for (idx = 0; idx < ht->size; idx++)
 	{
 		node = ht->array[idx];
 		while (node != NULL)
 		{
-			printf("'%s': '%s'", node->key, node->value);
+			printf("%s'%s': '%s'", blank,  node->key, node->value);
+			blank = ", ";
 			node = node->next;
-			if (node != NULL)
-				printf(", ");
 		}
 	}
 	printf("}\n");
